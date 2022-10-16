a08c1a1bf28b72b6b2e39c39b4cbb6e8be011577
ChaitanyaNirf <87872295+ChaitanyaNirf@users.noreply.github.com>
2022-10-16 16:02:26 +0530

    Create Find power of a number.java

---
+++ Lecture 4: Conditionals and Loops/Find power of a number.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Solution {
+    
+    public static void main(String[] args) {
+        // Write your code here
+        Scanner scanner = new Scanner(System.in);
+        
+        int x = scanner.nextInt();
+        int y = scanner.nextInt();
+        System.out.println((int) Math.pow(x,y));
+    }
+}
