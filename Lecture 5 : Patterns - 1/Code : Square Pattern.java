b012521cb7f2e7018330dcb15c971f08c9ec7fc7
ChaitanyaNirf <87872295+ChaitanyaNirf@users.noreply.github.com>
2022-10-16 16:03:30 +0530

    Create Code : Square Pattern.java

---
+++ Lecture 5 : Patterns - 1/Code : Square Pattern.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class Solution {
+
+
+	public static void main(String[] args) {
+		
+		/* Your class should be named Solution.
+	 	* Read input as specified in the question.
+	 	* Print output as specified in the question.
+		*/
+		
+        Scanner s = new Scanner(System.in);
+        int input = s.nextInt();
+        
+        for(int i=0;i<input;i++){
+            for(int j=0;j<input;j++){
+                System.out.print(input);
+            }
+            System.out.println();
+        }
+		
+	}
+
+}
